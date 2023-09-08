60c6e6ad7d6d68a10e1b42c96177c905ac3fa2bf
mehmetCelenk <95227625+mehmeCelenk@users.noreply.github.com>
2023-09-08 22:48:28 +0300

    Add files via upload

---
+++ Kodluyoruz Yaz Challenge/challenge12/Easy.java
@@ -0,0 +1,13 @@
+package challenge12;
+
+public class Easy {
+    public static void main(String[] args) {
+        int hiz = 60;
+        int mesafe = 240;
+
+
+        int zaman = mesafe / hiz;
+
+        System.out.println("Araba "  + mesafe +  " km yol almak için "  + zaman +  " saat sürer.");
+    }
+}
